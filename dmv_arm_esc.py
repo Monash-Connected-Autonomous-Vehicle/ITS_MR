ab480956a714655c6f409ae4f09b12a3556e6d99
Saami Junaidi <saamijunaidi@gmail.com>
2022-12-14 11:45:52 +1100

    Codes for the manual control of the DMV (dmv_control is the main code)

--- dmv_arm_esc.py
+++ dmv_arm_esc.py
@@ -6,7 +6,7 @@
 import os     #importing os library so as to communicate with the system
 import time   #importing time library to make Rpi wait because its too impatient 
 #os.system ("sudo pigpiod") #Launching GPIO library
-time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+#time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
 import pigpio #importing GPIO library
 
 ESC=4  #Connect the ESC in this GPIO pin 
@@ -102,7 +102,7 @@ def arm(): #This is the arming procedure of an ESC
         time.sleep(1)
         pi.set_servo_pulsewidth(ESC, min_value)
         time.sleep(1)
-        control() 
+#         control() 
         
 def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
     pi.set_servo_pulsewidth(ESC, 0)
--- dmv_control.py
+++ dmv_control.py
@@ -9,18 +9,20 @@ import atexit
 
 # Servo is connected to GPIO pin 17 (Change this value corresponding to which GPIO pin it is connected to)
 SERVO_GPIO_PIN = 17
-ESC_GPIO_PIN = 27
+ESC_GPIO_PIN = 4
 MIN_SERVO_PWM = 650  # A pulsewidth of 650us will turn the steering full lock to the right
 MID_SERVO_PWM = 1150 # A pulsewidth of 1150us will turn the steering to the neutral position
 MAX_SERVO_PWM = 1650 # A pulsewidth of 1650us will turn the steering full lock to the left
-MIN_ESC_PWM = 700
-MAX_ESC_PWM = 2000
+MIN_ESC_PWM = 1350   # A pulsewidth of 1350us is the max decceleration of the DMV
+MID_ESC_PWM = 1500   # A pulsewidth of 1500us is the neutral position of the DMV
+MAX_ESC_PWM = 1650   # A pulsewidth of 1650us is the max acceleration of the DMV
+
 # Assigning integer values to each key
 NONE = 0
 LEFT_ARROW = 1
 RIGHT_ARROW = 2
-#UP_ARROW = 3
-#DOWN_ARROW = 4
+UP_ARROW = 3
+DOWN_ARROW = 4
 HOME = 5
 QUIT = 6
 
@@ -41,6 +43,12 @@ def getch():
         in_escape = False
     elif char == 68 and in_cursor: # ASCII value for 'right arrow' key
         key = RIGHT_ARROW
+        in_escape = False
+    elif char == 65 and in_cursor: # ASCII value for 'up arrow' key
+        key = UP_ARROW
+        in_escape = False
+    elif char == 66 and in_cursor: # ASCII value for 'down arrow' key
+        key = DOWN_ARROW
         in_escape = False
     elif char == 104 and in_cursor: # ASCII value for 'h' key
         key = HOME
@@ -73,30 +81,11 @@ atexit.register(cleanup)
 in_escape = False
 in_cursor = False
 
-pwm = MID_SERVO_PWM
-pi.set_servo_pulsewidth(SERVO_GPIO_PIN, pwm)
+pwm_servo = MID_SERVO_PWM
+pwm_esc = MID_ESC_PWM
+pi.set_servo_pulsewidth(SERVO_GPIO_PIN, pwm_servo)
+pi.set_servo_pulsewidth(ESC_GPIO_PIN, pwm_esc)
 
-# CALIBRATING AND ARMING THE ESC
-#pi.set_servo_pulsewidth(ESC, 0)
-#print("Disconnect the battery and press Enter")
-#inp = input()
-#if inp == '':
-#    pi.set_servo_pulsewidth(ESC, max_value)
-#    print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
-#    inp = input()
-#    if inp == '':            
-#        pi.set_servo_pulsewidth(ESC, min_value)
-#        print ("Wierd eh! Special tone")
-#        time.sleep(7)  
-#        print ("Wait for it ....")
-#        time.sleep (5)
-#        print ("Im working on it, DONT WORRY JUST WAIT.....")
-#        pi.set_servo_pulsewidth(ESC, 0)
-#        time.sleep(2)
-#        print ("Arming ESC now...")
-#        pi.set_servo_pulsewidth(ESC, min_value)
-#        time.sleep(1)
-#        print ("See.... uhhhhh")
 
 while True and range(650, 1650):
     time.sleep(0.01)
@@ -104,18 +93,29 @@ while True and range(650, 1650):
 
     if char == QUIT:
         break
-    pulsewidth = pwm
-
-    if char == HOME:
-        pulsewidth = MID_SERVO_PWM # Stop
+    pulsewidth_servo = pwm_servo
+    pulsewidth_esc = pwm_esc
+     if char == HOME:
+        pulsewidth_servo = MID_SERVO_PWM # Neutral steering position
+        pulsewidth_esc = MID_ESC_PWM # Zero velocity
     elif char == LEFT_ARROW:
-        pulsewidth = pulsewidth - 100 # Shorten pulse
-        if pulsewidth < MIN_SERVO_PWM:
-            pulsewidth = MAX_SERVO_PWM
+        pulsewidth_servo = pulsewidth_servo - 100 # Shorten pulse
+        if pulsewidth_servo < MIN_SERVO_PWM:
+            pulsewidth_servo = pulsewidth_servo + 100
     elif char == RIGHT_ARROW:
-        pulsewidth = pulsewidth + 100 # Lengthen pulse
-        if pulsewidth > MAX_SERVO_PWM:
-            pulsewidth = MIN_SERVO_PWM
-    if pulsewidth != pwm:
-        pwm = pulsewidth
-        pi.set_servo_pulsewidth(SERVO_GPIO_PIN, pwm)
+        pulsewidth_servo = pulsewidth_servo + 100 # Lengthen pulse
+        if pulsewidth_servo > MAX_SERVO_PWM:
+            pulsewidth_servo = pulsewidth_servo - 100
+    elif char == UP_ARROW:
+        pulsewidth_esc = pulsewidth_esc + 50 # Lengthen pulse
+        if pulsewidth_esc > MAX_ESC_PWM:
+       elif char == DOWN_ARROW:
+        pulsewidth_esc = pulsewidth_esc - 50 # Shorten pulse
+        if pulsewidth_esc < MIN_ESC_PWM:
+            pulsewidth_esc = pulsewidth_esc + 50
+    if pulsewidth_servo != pwm_servo or pulsewidth_esc != pwm_esc:
+        pwm_servo = pulsewidth_servo
+        pwm_esc = pulsewidth_esc
+        pi.set_servo_pulsewidth(SERVO_GPIO_PIN, pwm_servo)
+        pi.set_servo_pulsewidth(ESC_GPIO_PIN, pwm_esc)
+        
