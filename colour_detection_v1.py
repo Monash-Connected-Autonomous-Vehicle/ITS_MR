d67a76da26aea2e82e0e09aacffa5856d80abd57
SAAMI JUNAIDI <saamijunaidi@gmail.com>
2023-02-07 15:12:55 +1100

    Adding code to find coordinates of the filtered mask and the center of it aswell

--- colour_detection_v1.py
+++ colour_detection_v1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 video = cv2.VideoCapture(0)
+coords = []
 
 while True:
     _, frame = video.read()
@@ -12,6 +13,7 @@ while True:
     high_red = np.array([179, 255, 255])
     red_mask = cv2.inRange(frameHSV, low_red, high_red)
     red = cv2.bitwise_and(frame, frame, mask = red_mask)
+    last_x_red, last_y_red = 0, 0 # Store the last x and y red positions
 
     # Every colour except white
     low = np.array([0, 42, 0])
@@ -22,6 +24,27 @@ while True:
     cv2.imshow("Frame", frame)
     cv2.imshow("Red mask", red)
     cv2.imshow("Result", result)
+
+    # Find coordinates
+    rows, cols, _ = result.shape
+    for x in range(rows):
+        for y in range(cols):
+            px = list(result[x, y])
+            if px == red_mask:
+                if len(coords) == 0:
+                    coords.append((y, x))
+
+                last_x_red, last_y_red = x, y
+    
+    coords.append((last_y_red, last_x_red))
+    print(coords)
+    top_left = coords[0]
+    bottom_left = (coords[0][0], coords[1][1])
+    top_right = (coords[1][0], coords[0][1])
+    bottom_right = coords[1]
+    center = (top_right[0] - top_left[0], bottom_right[1] - top_right[1])
+    print('The coordinates of the mask from left to right and top to bottom and also the center are:')
+    print(f'Top Left: {top_left}, Top Right: {top_right}, Bottom Left: {bottom_left}, Bottom Right: {bottom_right}, Center: {center}')
     
     key = cv2.waitKey(1)
     if key == 27:  # "s" key
